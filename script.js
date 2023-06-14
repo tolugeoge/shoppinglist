9c56ce78531a868b33318e6d5faf143c594b30fd
Tolu George <2olu4real@gmail.com>
2023-06-14 13:02:37 +0100

    Filter or Search parameters updates

--- script.js
+++ script.js
@@ -60,6 +60,21 @@ function clearItems(e) {
   checkUI();
 }
 
+function filterItems(e) {
+  const items = itemList.querySelectorAll("li");
+  const text = e.target.value.toLowerCase();
+  console.log(text);
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = "flex";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
+
 function checkUI() {
   const items = itemList.querySelectorAll("li");
   if (items.length === 0) {
@@ -75,5 +90,6 @@ function checkUI() {
 itemForm.addEventListener("submit", addItem);
 itemList.addEventListener("click", removeItem);
 clearBtn.addEventListener("click", clearItems);
+itemFilter.addEventListener("input", filterItems);
 
 checkUI();
